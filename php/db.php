1a0d2bf29e085cecafe14e50834915ec37e0ee96
Subhash R <subhashsanju2003@gmail.com>
2026-09-04 17:53:59 +0530

    Add Dockerfile for Render deployment

--- php/db.php
+++ php/db.php
@@ -3,8 +3,17 @@ $host = getenv('DB_HOST') ?: 'localhost';
 $db   = getenv('DB_NAME') ?: 'auth_system';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
+$port = getenv('DB_PORT') ?: 3306;
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+$mysqli = mysqli_init();
+
+// Use SSL if connecting to a cloud host (Aiven requires it)
+if (getenv('DB_HOST')) {
+    $mysqli->ssl_set(null, null, null, null, null);
+    $mysqli->real_connect($host, $user, $pass, $db, $port, null, MYSQLI_CLIENT_SSL);
+} else {
+    $mysqli = new mysqli($host, $user, $pass, $db, $port);
+}
 
 if ($mysqli->connect_error) {
     die(json_encode([
